56cc4e3f1be11589895815c568fbd83bafdbe17b
shaham150 <efrat.shaham@protonmail.com>
2025-05-30 10:24:58 -0400

    Full Lecture 5 example code-along

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// Script
+// AngularJS script
+
+( function () {
+  'use strict';
+  angular.module("myApp", [])
+    .controller("myController", function ($scope) {
+      $scope.name = "";
+    }); // Define Angular module, with no dependencies
+
+})();
