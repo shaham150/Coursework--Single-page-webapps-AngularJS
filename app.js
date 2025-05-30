fbaba9b09b23cd7d995c4d1963e99e29314b7c30
shaham150 <efrat.shaham@protonmail.com>
2025-05-30 12:02:55 -0400

    Full lecture6 example code-along

--- app.js
+++ app.js
@@ -2,9 +2,27 @@
 
 ( function () {
   'use strict';
-  angular.module("myApp", [])
-    .controller("myController", function ($scope) {
-      // ...
-    }); // Define Angular module, with no dependencies
+  angular.module("nameCalc", [])
+    .controller("nameCalcContr", function ($scope) {
+      $scope.name = ""; // Initialize
+      $scope.totalValue = 0; // Initialize
+      
+      $scope.displayNumeric = function (){
+        var numericVal = calcNumeric($scope.name);
+        $scope.totalValue = numericVal;
+      }
+
+     function calcNumeric(s){
+       var total = 0; // Iniotialize
+
+       // Calculate running total of numeric value:
+       for (var i=0; i<s.length; i++){
+          total += s.charCodeAt(i);
+        }
+       
+       return total;
+     } // End function
+    
+    }); // End controller definition
 
 })();
