257028dc6f823fd54c8a615b9e6f6bddf0aa40f3
shaham150 <efrat.shaham@protonmail.com>
2025-05-31 13:13:29 -0400

    Full lecture9 example code-along

--- app.js
+++ app.js
@@ -4,7 +4,12 @@
   angular.module('DIApp', [])
     .controller('DIController', DIController); // Controller calls external function
     
-    function DIController ($scope) {
+    function DIController ($scope, $filter) {
       $scope.name = "Yaakov";
+      
+      $scope.upper = function () {
+        var upCase = $filter("uppercase"); // Function to filter by uppercase letters
+        $scope.name = upCase($scope.name)
+      };
     }
 })();
