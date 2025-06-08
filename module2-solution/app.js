a4e60859968396042215aa3735f86b9994171a84
shaham150 <efrat.shaham@protonmail.com>
2025-06-08 18:30:31 -0400

    Created skeleton app.js and connected Angular.js to HTML

---
+++ module2-solution/app.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict';
+
+    angular.module("ShoppingListCheckOff", [])
+        .controller("ToBuyController", ToBuyController)
+        .controller("AlreadyBoughtController", AlreadyBoughtController)
+})();
