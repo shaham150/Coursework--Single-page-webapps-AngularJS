32a443b598cc9aa9559354bf83a9a9ed8b37beb4
shaham150 <efrat.shaham@protonmail.com>
2025-06-17 22:28:21 -0400

    Finished controller and menu-search service

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,13 +1,24 @@
 (function () {
     'use strict';
 
-    angular.module("NarrowItDownApp", []")
+    angular.module("NarrowItDownApp", [])
         .controller("NarrowItDownController", NarrowItDownController)
-        .service("MenuSearchService", MenuSearchService);
+        .service("MenuSearchService", MenuSearchService)
+        .directive("foundItems", foundItems);
+    
     
     NarrowItDownController.$inject = ["MenuSearchService"];
-    function NarrowItDownController("MenuSearchService"){
-        //
+    function NarrowItDownController(MenuSearchService){
+        var ctrl = this;
+
+        ctrl.setSearchTerm = function () {
+            // Retrieve the list of matching menu items based on the user's search term:
+            ctrl.found = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm);
+
+            // console.log("CLICKED:", );
+        }
+
+        return;
     }
 
     MenuSearchService.$inject = ["$http"];
@@ -16,16 +27,38 @@
 
         service.getMatchedMenuItems = function (searchTerm) {
             var httpObj = {
-                url: "",
-                params: {}
+                url: "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json",
             }
 
             return $http(httpObj).then(function (result) {
+                // Create array to hold matching menu items:
+                var foundItems = [];
 
+                var resultData = result.data;
+
+                // Loop over the categories of menu items retrieved from server:
+                for (let category of Object.entries(resultData)){
+
+                    var categoryItems = category[1].menu_items; // Grab the items from the given category
+                    
+                    // For each category, loop over its menu items:
+                    for (let menuItem of Object.entries(categoryItems)) {
+                        var menuItemDescription = menuItem[1].description; // Grab the item description from the object
+
+                        // If the menu-item description contains the search term, add it to the list:
+                        if (menuItemDescription.indexOf(searchTerm) != -1) {
+                            foundItems.push(menuItem);
+                        }
+                    }
+                }
+
+                console.log("FOUND ITEMS:",foundItems);
             }).catch(function (error) {
-                console.log(error);
+                console.log("Error in HTTP request:", error);
             });
         }
 
-    }
+    } // End of controller
+
+    
 })();
