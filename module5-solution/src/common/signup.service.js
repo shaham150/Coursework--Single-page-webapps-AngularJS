77e8c3dcb227c98556a1b3c86ffff4ca4b5c1910
shaham150 <efrat.shaham@protonmail.com>
2025-07-03 18:26:41 -0400

    Finished all sign-up page functionality

--- module5-solution/src/common/signup.service.js
+++ module5-solution/src/common/signup.service.js
@@ -8,24 +8,67 @@
     function SignupService ($http, ApiPath) {
         var service = this;
 
+        function parseMenuCode (menuCode) {
+            var parsedCode = {
+                menu: "",
+                item: ""
+            }
+
+            for (let char of menuCode) {
+                if ( !(char in [0,1,2,3,4,5,6,7,8,9]) ) {
+                    // If the current char is NOT a number, add it to the "menu" attribute:
+                    parsedCode.menu += char;
+                    console.log(parsedCode.menu);
+                } else if (char in [0,1,2,3,4,5,6,7,8,9]) {
+                    parsedCode.item += char;
+                    console.log(parsedCode.item);
+                }
+            }
+
+            console.log("parsed:", parsedCode);
+            return parsedCode;
+        }
+
         service.getFaveItem = function (itemCode) {
             console.log("FAVE ITEM CODE IS: ", itemCode);
+
+            var parsedItemCode = parseMenuCode(itemCode);
+            console.log("PARSED", parsedItemCode);
+
             return $http.get("https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json").then(function (response) {
                 console.log(response.data);
 
+                // Look for a menu categorey match:
                 for (let ctgry in response.data) {
-                    var currentCat = response.data[ctgry].category.short_name;
-                    console.log(currentCat);
+                    // Gather category and short-name info from server response:
+                    var currentCat = response.data[ctgry];
+                    var currentCode = currentCat.category.short_name;
+                    console.log(currentCode);
 
-                    if (currentCat.toLowerCase() == itemCode) {
-                        console.log("MATCH FOUND");
-                        service.faveMenuItem = faveItem; // Store fave item for later use
-                        return faveItem;
+
+                    // If a match is found with the menu that the user specified, then look for a match to the item number specified:
+                    if (currentCode.toLowerCase() == parsedItemCode.menu.toLowerCase()) {
+                        console.log("CATEGORY MATCH FOUND ", currentCat.menu_items);
+
+                        for (let currentItem of currentCat.menu_items){
+                            // Check for a match between the current item number and the item number that the user specified:
+                            if (parseMenuCode(currentItem.short_name).item == parsedItemCode.item){
+                                service.faveMenuItem = currentItem; // Store fave item for later use
+                                console.log("match found:", currentItem);
+                                return;
+                            }
+                        }
+
+                        // Store fave item as null if no item-number match was found:
+                        console.log("no match");
+                        service.faveMenuItem = 0;
+                        return;
                     }
                 }
 
+                // Store fave item as null if no menu-category match was found:
                 console.log("no match");
-                service.faveMenuItem = null;                
+                service.faveMenuItem = 0;                
             });
         }
     }
--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -9,9 +9,5 @@
         var $ctrl = this;
 
         $ctrl.SignupService = SignupService;
-
-        $ctrl.test = function () {
-            console.log("THIS CONTROLLER WORKS");
-        }
     }
 })();
